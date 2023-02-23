1c4e5638352df3815ac6b3e206b13f67413a31af
yessineblanco <yessine.blanco@esprit.tn>
2023-02-23 21:48:37 +0100

    Add::Recherche&pdf

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,31 +3,104 @@
 namespace App\Controller;
 
 use App\Entity\Product;
-use App\Entity\ProductImages;
 use App\Form\ProductType;
 use App\Repository\ProductRepository;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
 
 #[Route('/product')]
 class ProductController extends AbstractController
 {
     #[Route('/', name: 'app_product_index', methods: ['GET'])]
-    public function index(ProductRepository $productRepository): Response
+    public function index(PaginatorInterface $paginator,ProductRepository $productRepository,Request $request): Response
     {
+        $data=$productRepository->findAll();
+        $Produits=$paginator->paginate(
+            $data,
+            $request->query->getInt('page',1),
+            5
+
+        );
         return $this->render('product/cards.html.twig', [
-            'products' => $productRepository->findAll(),
+            'products' => $Produits
+
         ]);
+    }
+    #[Route('/prods/pdf', name: 'pdfp', methods: ['GET'])]
+    public function pdfd (ProductRepository $productRepository,Request $request): Response
+    {
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+//        $pdfOptions->set('defaultFont', 'Arial');
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+
+//        $produit = $produitRepository->findAll();
+
+        // Retrieve the HTML generated in our twig file
+        $data=$productRepository->findAll();
+        $html = $this->renderView('product/pdf.html.twig',[
+            'products' => $data,
+        ]);
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (force download)
+        $dompdf->stream("Product.pdf", [
+            "Attachment" => true
+        ]);
+        return new Response('', 200, [
+            'Content-Type' => 'application/pdf',
+        ]);
+    }
+    #[Route('/prod/searchp', name: 'search_product', methods: ['GET'])]
+    public function searchprod(Request $request, NormalizerInterface $Normalizer, PaginatorInterface $paginator)
+    {
+        $Produits=$paginator->paginate(
+            $this->getDoctrine()->getRepository(Product::class)->findBy(['ProductName' => $request->get('search')]),
+            $request->query->getInt('page',1),
+            4
+
+        );
+        dump($request->get('search'));
+        if (null != $request->get('search')) {
+            return $this->render('/product/index.html.twig', [
+                'products' => $Produits,'user'=>$this->getUser(),
+            ]);
+        }
+        return  $this->redirectToRoute('produit_index');
     }
     #[Route('/back', name: 'app_product_index_back', methods: ['GET'])]
-    public function indexback(ProductRepository $productRepository): Response
+    public function indexback(PaginatorInterface $paginator,ProductRepository $productRepository,Request $request): Response
     {
+        $data=$productRepository->findAll();
+        $Produits=$paginator->paginate(
+            $data,
+            $request->query->getInt('page',1),
+            5
+
+        );
         return $this->render('product/index.html.twig', [
-            'products' => $productRepository->findAll(),
+            'products' => $Produits
+
         ]);
     }
+
     #[Route('/new', name: 'app_product_new', methods: ['GET', 'POST'])]
     public function new(Request $request, ProductRepository $productRepository): Response
     {
